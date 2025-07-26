e76fb633e08b748efe5278f0f59b556145bbeb0e
ssaruhan <110460948+ssaruhan@users.noreply.github.com>
2025-07-26 22:23:12 +0100

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	language: "en",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Sam Saruhan",
+		email: "samsaruhan2000@gmail.com",
+		url: "https://ssaruhan.com/"
 	}
 }
